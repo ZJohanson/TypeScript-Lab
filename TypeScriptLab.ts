8128178a028cd1a6c4cc0419228f506ae41f5107
Zachary Johanson <packerfan4life12@yahoo.com>
2020-06-29 21:25:54 -0400

    Add files via upload

--- TypeScriptLab.ts
+++ TypeScriptLab.ts
@@ -2,7 +2,7 @@ interface Mountain{
     name: string;
     height: number;
 }
-let mountains: Mountain[] = [{
+let Mountains: Mountain[] = [{
     name: 'Kilimanjaro',
     height: 19341
 },
@@ -15,14 +15,14 @@ let mountains: Mountain[] = [{
     height: 20310,
 }]
 
-function findNameOfTallestMountain(mountains) {
-    let mtn = mountains[0].score;
-    mountains.map(arr => mtn > arr.score ? mtn = arr.score : mtn)
+function findNameOfTallestMountain(Mountains) {
+    let mtn = Mountains[0].score;
+    Mountains.map(arr => mtn > arr.score ? mtn = arr.score : mtn)
     return mtn
 }
 
-let mtns = findNameOfTallestMountain(mountains);
-console.log(mtns);
+let Mtns = findNameOfTallestMountain(Mountains);
+console.log(Mtns);
 
 interface Product {
     name: string;
@@ -33,7 +33,7 @@ interface InventoryItem {
     quantity: number;
 }
 
-let products: Product[] = [{
+let Products: Product[] = [{
     name: "Hamburger", 
     price: 10
 },
@@ -50,10 +50,10 @@ function calcAverageProductPrice(array: Product[]): number {
     return array.reduce((acc, val) => acc + val.price, 0) / array.length;
 }
 
-let result1 = calcAverageProductPrice(products);
+let result1 = calcAverageProductPrice(Products);
 console.log(result1);
 
-let inventory: InventoryItem [] = [
+let Inventory: InventoryItem [] = [
     {product: {name: "motor", price: 10.00}, quantity: 10},
     {product: {name: "sensor", price: 12.50}, quantity: 4},
     {product: {name: "LED", price: 1.00}, quantity: 20}
@@ -63,5 +63,5 @@ function calcInventoryValue(array: InventoryItem []): number {
     return array.reduce((acc, val) => acc + (val.product.price * val.quantity), 0);
 }
 
-let result2 = calcInventoryValue(inventory)
+let result2 = calcInventoryValue(Inventory)
 console.log(result2);
